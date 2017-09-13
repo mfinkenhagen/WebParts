25bde43d49691ee61f77639355535a34dc00a0d3
Mark Finkenhagen <MFINKENHAGEN@sw.org>
2017-09-13 16:22:32 -0500

    first commit

---
+++ src/webparts/helloWorld/HelloWorldWebPart.ts
@@ -0,0 +1,121 @@
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField,
+  PropertyPaneCheckbox,
+  PropertyPaneDropdown,
+  PropertyPaneToggle
+} from '@microsoft/sp-webpart-base';
+
+import {
+  Environment,
+  EnvironmentType
+} from '@microsoft/sp-core-library';
+
+import { escape } from '@microsoft/sp-lodash-subset';
+
+import styles from './HelloWorld.module.scss';
+import * as strings from 'helloWorldStrings';
+import { IHelloWorldWebPartProps } from './IHelloWorldWebPartProps';
+import MockHttpCllient from './MockHttpClient';
+import {
+  SPHttpClient,
+  SPHttpClientResponse   
+} from '@microsoft/sp-http';
+
+
+export interface ISPLists{
+  value: ISPList[];
+}
+export interface ISPList{
+  Title: string;
+  Id: string;
+}
+export default class HelloWorldWebPart extends BaseClientSideWebPart<IHelloWorldWebPartProps> {
+
+  private _getMocklistData(): Promise<ISPLists>{
+    return MockHttpCllient.get().then((data: ISPList[]) => {
+      var listData: ISPLists = {value: data};
+      return listData;
+    }) as Promise<ISPLists>;
+  }
+
+  private _getListData(): Promise<ISPLists> {
+    return this.context.spHttpClient.get(this.context.pageContext.web.absoluteUrl + `/_api/web/lists?$filter=Hidden eq false`, SPHttpClient.configurations.v1)
+      .then((response: SPHttpClientResponse) => {
+        return response.json();
+      });
+  }
+
+
+
+
+
+
+  public render(): void {
+    this.domElement.innerHTML = `
+      <div class="${styles.helloWorld}">
+        <div class="${styles.container}">
+          <div class="ms-Grid-row ms-bgColor-themeDark ms-fontColor-white ${styles.row}">
+            <div class="ms-Grid-col ms-u-lg10 ms-u-xl8 ms-u-xlPush2 ms-u-lgPush1">
+              <span class="ms-font-xl ms-fontColor-white">Welcome to SharePoint!</span>
+              <p class="ms-font-l ms-fontColor-white">Customize SharePoint experiences using Web Parts.</p>
+              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.description)}</p>
+              <p class="ms-font-l ms-fontColor-white">${escape(this.properties.test2)}</p>
+              <p class="ms-font-l ms-fontColor-white">Loading from ${escape(this.context.pageContext.web.title)}</p>
+              <a href="https://aka.ms/spfx" class="${styles.button}">
+                <span class="${styles.label}">Learn more</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+     return {
+    pages: [
+      {
+        header: {
+          description: strings.PropertyPaneDescription
+        },
+        groups: [
+          {
+            groupName: strings.BasicGroupName,
+            groupFields: [
+            PropertyPaneTextField('description', {
+              label: 'Description'
+            }),
+            PropertyPaneTextField('test', {
+              label: 'Multi-line Text Field',
+              multiline: true
+            }),
+            PropertyPaneCheckbox('test1', {
+              text: 'Checkbox'
+            }),
+            PropertyPaneDropdown('test2', {
+              label: 'Dropdown',
+              options: [
+                { key: '1', text: 'One' },
+                { key: '2', text: 'Two' },
+                { key: '3', text: 'Three' },
+                { key: '4', text: 'Four' }
+              ]}),
+            PropertyPaneToggle('test3', {
+              label: 'Toggle',
+              onText: 'On',
+              offText: 'Off'
+            })
+          ]
+          }
+        ]
+      }
+    ]
+  };
+  }
+}
